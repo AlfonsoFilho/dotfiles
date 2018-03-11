cc969eb52dde28c2662fb26d1963147115f9b739
Alfonso Filho <alfonsocfilho@gmail.com>
2018-03-11 12:38:38 +0000

    enabled bash autocomplete

--- bashrc.bash
+++ bashrc.bash
@@ -4,6 +4,10 @@ case $- in
       *) return;;
 esac
 
+if [ -f /etc/bash_completion ]; then
+. /etc/bash_completion
+fi
+
 if [ -d $HOME/.dotfiles/bash.d ]; then
   for file in $HOME/.dotfiles/bash.d/*.bash; do
     source $file
