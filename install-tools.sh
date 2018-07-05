eae9872dff7ba3d545950921f9858f8d00abeaaf
Alfonso Filho <alfonsocfilho@gmail.com>
2018-07-05 22:14:43 +0100

    updated to zsh

--- install-tools.sh
+++ install-tools.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-apt-get update -yq
+# sudo rm -rf $HOME/.zim
 
-apt-get install -yq \
+sudo apt-get update -yq
+
+# Terminal Tools
+sudo apt-get install -yq \
+  git \
+  zsh \
   wget \
   curl \
   tig \
@@ -18,9 +23,38 @@ apt-get install -yq \
   xclip \
   shellcheck
 
+# Zsh
+# chsh -s $(which zsh)
 
-#
-# icdiff
-#
+# GUI apps
+sudo apt-get install -yq \
+  gimp \
+  inkscape \
+  code \
+  code-insiders
+
+# Git icdiff
 curl -s https://raw.githubusercontent.com/jeffkaufman/icdiff/release-1.9.0/icdiff | sudo tee /usr/local/bin/icdiff > /dev/null 
 sudo chmod ugo+rx /usr/local/bin/icdiff
+
+# NVM
+# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+# Zim
+# zsh zsh-install.zsh
+
+# Rust
+# curl https://sh.rustup.rs -sSf | sh
+
+# # Alacritty
+# git clone https://github.com/jwilm/alacritty.git
+# cd alacritty || exit
+# rustup override set stable
+# rustup update stable
+# source $HOME/.cargo/env
+# sudo apt-get install -yq cmake libfreetype6-dev libfontconfig1-dev
+# cargo build --release
+# cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+# desktop-file-install alacritty.desktop
+# update-desktop-database
+# cp alacritty-completions.zsh /usr/share/zsh/functions/Completion/X/_alacritty
