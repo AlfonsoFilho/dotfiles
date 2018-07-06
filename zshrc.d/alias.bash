79b8c9c6fc23a2c2d767dc630b788ac69465c326
Alfonso Filho <alfonsocfilho@gmail.com>
2018-07-06 18:38:20 +0100

    added some alias

--- zshrc.d/alias.bash
+++ zshrc.d/alias.bash
@@ -13,6 +13,9 @@ alias cl="clear"
 alias ls="command ls --color"
 alias la="ls -lahFog"
 alias mkdir="mkdir -p"
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
 
 # Print each PATH entry on a separate line
 alias path='echo -e ${PATH//:/\\n}'
@@ -24,3 +27,9 @@ alias gd="git difftool --no-prompt --extcmd \"icdiff -NH\""
 alias gl="git log --oneline --decorate --all --graph"
 alias ga="git add ."
 alias gcm="git commit"
+
+# Misc
+alias findHere="find . -iname "
+alias psg="ps aux | grep -v grep | grep -i -e VSZ -e"
+alias myip="curl http://ipecho.net/plain; echo"
+alias top-commands='history | awk "{print $2}" | awk "{print $1}" |sort|uniq -c | sort -rn | head -10'
