6969b423934bd455e830ba1c1f4c8a7dc5f14625
Alfonso Filho <alfonsocfilho@gmail.com>
2019-05-01 22:39:54 +0100

    Removed committed code from install-tools and zimrc

--- install-tools.sh
+++ install-tools.sh
@@ -18,43 +18,24 @@ sudo apt-get install -yq \
   tmux \
   vim \
   ranger \
-  fish \
   neovim \
   xclip \
-  shellcheck
+  shellcheck \
+  fonts-symbola
 
 # Zsh
-# chsh -s $(which zsh)
+chsh -s $(which zsh)
 
 # GUI apps
 sudo apt-get install -yq \
   gimp \
   inkscape \
-  code \
-  code-insiders
+  code 
 
 # Git icdiff
 curl -s https://raw.githubusercontent.com/jeffkaufman/icdiff/release-1.9.0/icdiff | sudo tee /usr/local/bin/icdiff > /dev/null 
 sudo chmod ugo+rx /usr/local/bin/icdiff
 
 # NVM
-# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 
-# Zim
-# zsh zsh-install.zsh
-
-# Rust
-# curl https://sh.rustup.rs -sSf | sh
-
-# # Alacritty
-# git clone https://github.com/jwilm/alacritty.git
-# cd alacritty || exit
-# rustup override set stable
-# rustup update stable
-# source $HOME/.cargo/env
-# sudo apt-get install -yq cmake libfreetype6-dev libfontconfig1-dev
-# cargo build --release
-# cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
-# desktop-file-install alacritty.desktop
-# update-desktop-database
-# cp alacritty-completions.zsh /usr/share/zsh/functions/Completion/X/_alacritty
